fff3e828b0a3f20a12beccf9a055bb1e0283d6ba
Sachin Sardiwal <sachin@Sachins-MacBook-Air.local>
2018-05-16 00:38:07 +0700

    final update

--- tpuctanalysis-segmentation-and-classification-d1c0df68cbfc/dicom_data_working/filter_images.py
+++ tpuctanalysis-segmentation-and-classification-d1c0df68cbfc/dicom_data_working/filter_images.py
@@ -5,12 +5,11 @@ from scipy.misc.pilutil import Image
 
 
 
-image_path="/Users/sachin/Desktop/CT_Project/masked_dicom_images/000.png"
+image_path="/Users/sachin/Desktop/CT_Project/masked_dicom_images/026.png"
 a=Image.open(image_path)
 
-k=np.ones((5,5))/25
 
-b=scipy.ndimage.filters.median_filter(a,size=5,footprint=None,output=None,mode='reflect',cval=0.0,origin=0)
+b=scipy.ndimage.filters.median_filter(a,size=3,footprint=None,output=None,mode='reflect',cval=0.0,origin=0)
 
 b=scipy.misc.toimage(b)
-b.save("/Users/sachin/Desktop/CT_Project/masked_images_after_filter/000.png")
+b.save("/Users/sachin/Desktop/CT_Project/masked_images_after_filter/026_3_1.png")
