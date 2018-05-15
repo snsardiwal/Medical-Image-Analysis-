a65845beb6db4e38d76f91fe710f111569e3cd3d
Sachin Sardiwal <sachin@Sachins-MacBook-Air.local>
2018-05-16 01:04:09 +0700

    canny-filter added

--- tpuctanalysis-segmentation-and-classification-d1c0df68cbfc/dicom_data_working/filter_images.py
+++ tpuctanalysis-segmentation-and-classification-d1c0df68cbfc/dicom_data_working/filter_images.py
@@ -1,15 +1,15 @@
 import numpy as np 
 import scipy.ndimage
+from skimage import feature
 from scipy.misc.pilutil import Image
+import scipy.misc
 
 
 
 
 image_path="/Users/sachin/Desktop/CT_Project/masked_dicom_images/026.png"
 a=Image.open(image_path)
-
-
-b=scipy.ndimage.filters.median_filter(a,size=3,footprint=None,output=None,mode='reflect',cval=0.0,origin=0)
-
+a=scipy.misc.fromimage(a)
+b=feature.canny(a,sigma=1.0)
 b=scipy.misc.toimage(b)
-b.save("/Users/sachin/Desktop/CT_Project/masked_images_after_filter/026_3_1.png")
+b.save("/Users/sachin/Desktop/CT_Project/masked_images_after_filter/026_1.png")
